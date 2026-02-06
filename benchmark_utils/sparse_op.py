78ea18b0a7bd9fe2916d611ae73f9404482b7b6b
damien <damien.lesens@orange.fr>
2026-02-06 09:49:35 +0100

    keeping coo format for matrix products, sparse matrix type is a parameter of the function

--- benchmark_utils/sparse_op.py
+++ benchmark_utils/sparse_op.py
@@ -1,13 +1,18 @@
 import scipy.sparse as sp
-from scipy.sparse import coo_array
+from scipy.sparse import coo_array,csr_array,csc_array
 import numpy as np
 
-def VoverWH(V,W,H):
+def VoverWH(V,W,H,type="csr"):
     i,j = V.nonzero()
     WHdata = np.einsum('ik,ik->i', W[i], H.T[j])
     eps=np.finfo(float).eps
     Qdata = V.data/(WHdata+eps)
-    Q = coo_array((Qdata,(i,j)),shape=V.shape)
+    if type=="coo":
+        Q = coo_array((Qdata,(i,j)),shape=V.shape)
+    elif type=="csr":
+        Q = csr_array((Qdata,(i,j)),shape=V.shape)
+    else:
+        Q = csc_array((Qdata,(i,j)),shape=V.shape)
     return Q
 
 def VoverWH2(V,W,H):
@@ -15,5 +20,10 @@ def VoverWH2(V,W,H):
     WHdata = np.einsum('ik,ik->i', W[i], H.T[j])
     eps=np.finfo(float).eps
     Qdata = V.data/(WHdata**2+eps)
-    Q = coo_array((Qdata,(i,j)),shape=V.shape)
+    if type=="coo":
+        Q = coo_array((Qdata,(i,j)),shape=V.shape)
+    elif type=="csr":
+        Q = csr_array((Qdata,(i,j)),shape=V.shape)
+    else:
+        Q = csc_array((Qdata,(i,j)),shape=V.shape)
     return Q
--- solvers/mu.py
+++ solvers/mu.py
@@ -53,7 +53,7 @@ class Solver(BaseSolver):
             for _ in range(n_inner_iter):
                 
                 if sp.issparse(self.X):
-                    Q = VoverWH(self.X,self.W,self.H)
+                    Q = VoverWH(self.X,self.W,self.H,'coo')
                     self.W = self.W * (Q @ self.H.T)/(oneHT+eps)
                 else:
                     self.W = self.W * ((self.X/(self.W@self.H+eps))@self.H.T)/(oneHT+eps)
@@ -63,7 +63,7 @@ class Solver(BaseSolver):
             for _ in range(n_inner_iter):
                 
                 if sp.issparse(self.X):
-                    Q = VoverWH(self.X,self.W,self.H)
+                    Q = VoverWH(self.X,self.W,self.H,'coo')
                     self.H = self.H * (self.W.T @ Q)/(WT1+eps)
                 else:
                     self.H = self.H * (self.W.T @ (self.X/(self.W@self.H+eps)))/(WT1+eps)
