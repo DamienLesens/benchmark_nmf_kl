69be2eb75f75271194083c57ff2e43ded82fd1c8
damien <damien.lesens@orange.fr>
2026-02-06 15:13:47 +0100

    sparse code scalar newton

--- solvers/scalar_newton.py
+++ solvers/scalar_newton.py
@@ -1,5 +1,7 @@
 from benchopt import BaseSolver, safe_import_context
 from benchopt.stopping_criterion import SufficientProgressCriterion,NoCriterion
+import scipy.sparse as sp
+from benchmark_utils.sparse_op import VoverWH,VoverWH2
 
 with safe_import_context() as import_ctx:
     import numpy as np
@@ -33,20 +35,31 @@ class Solver(BaseSolver):
         R = self.rank
         D = self.n_inner_iter
 
+        eps=np.finfo(float).eps
+
         if not self.factors_init:
             # Random init if init is not provided
             self.W, self.H = [np.random.rand(N, R), np.random.rand(R, M)]
         else:
             self.W, self.H = [np.copy(self.factors_init[i]) for i in range(2)]
         
-        WH = self.W @ self.H
-
-        eps=np.finfo(float).eps
+        if sp.issparse(self.X):
+            i,j = self.X.nonzero()
+            WHdata = np.maximum(np.einsum('ik,ik->i', self.W[i], self.H.T[j]),eps)
+        else:
+            WH = np.maximum(self.W @ self.H,eps)
         
         #computing self-concordance constants
         if self.method == "SN":
-            chj = np.max((self.X > 0) / np.sqrt(self.X), axis=0)
-            cwi = np.max((self.X > 0) / np.sqrt(self.X), axis=1)
+            if sp.issparse(self.X):
+                vals = 1.0 / np.sqrt(self.X.data)
+                chj = np.zeros(self.X.shape[1])
+                np.maximum.at(chj, self.X.col, vals)
+                cwi = np.zeros(self.X.shape[0])
+                np.maximum.at(cwi, self.X.row, vals)
+            else:
+                chj = np.max((self.X > 0) / np.sqrt(self.X), axis=0)
+                cwi = np.max((self.X > 0) / np.sqrt(self.X), axis=1)
 
         while callback():
 
@@ -58,8 +71,17 @@ class Solver(BaseSolver):
                 Wnew = np.copy(self.W)
 
                 for q in range(R):
-                    grad = - (self.X/WH).dot(self.H[q, :]) + sum_H[q]
-                    hess = (self.X/WH**2).dot(self.H[q, :]**2)  # .T)
+
+                    if sp.issparse(self.X):
+                        # grad = - sp.csr_array((self.X.data/WHdata,(i,j)),shape=self.X.shape).dot(self.H[q, :]) + sum_H[q]
+                        tmp = (self.X.data / WHdata) * self.H[q, j]
+                        grad = -np.bincount(i, weights=tmp, minlength=N) + sum_H[q]
+                        # hess = sp.csr_array((self.X.data/(WHdata**2),(i,j)),shape=self.X.shape).dot(self.H[q, :]**2)
+                        tmp2 = (self.X.data / (WHdata**2)) * (self.H[q, j]**2)
+                        hess = np.bincount(i, weights=tmp2, minlength=N)
+                    else:
+                        grad = - (self.X/WH).dot(self.H[q, :]) + sum_H[q]
+                        hess = (self.X/WH**2).dot(self.H[q, :]**2)  
                     s = np.maximum(self.W[:, q] - grad/hess, eps)
                     if self.method == "SN":
                         # safe update
@@ -69,8 +91,12 @@ class Solver(BaseSolver):
                     else:
                         Wnew[:, q] = s
 
-                    WH += np.outer(Wnew[:, q] - self.W[:, q], self.H[q, :])  # updated
-                    WH = np.maximum(WH, eps)
+                    if sp.issparse(self.X):
+                        WHdata += (Wnew[:,q]-self.W[:,q])[i]*(self.H[q,:])[j]
+                        WHdata = np.maximum(WHdata, eps)
+                    else:
+                        WH += np.outer(Wnew[:, q] - self.W[:, q], self.H[q, :])  # updated
+                        WH = np.maximum(WH, eps)
                 
                 self.W = Wnew
             
@@ -82,8 +108,17 @@ class Solver(BaseSolver):
                 Hnew = np.copy(self.H)
                 
                 for q in range(R):
-                    grad = - (self.W[:, q]).dot(self.X/WH) + sum_W[q]
-                    hess = ((self.W[:, q]**2)).dot(self.X/(WH**2))  # elementwise 2d order derivative
+
+                    if sp.issparse(self.X):
+                        # grad = - (self.W[:, q]).dot(sp.csc_array((self.X.data/WHdata,(i,j)),shape=self.X.shape)) + sum_W[q]
+                        tmp = (self.X.data / WHdata) * self.W[i, q]
+                        grad = -np.bincount(j, weights=tmp, minlength=M) + sum_W[q]
+                        # hess = ((self.W[:, q]**2)).dot(sp.csc_array((self.X.data/(WHdata**2),(i,j)),shape=self.X.shape))  # elementwise 2d order derivative
+                        tmp2 = (self.X.data / (WHdata**2)) * (self.W[i, q]**2)
+                        hess = np.bincount(j, weights=tmp2, minlength=M)
+                    else:
+                        grad = - (self.W[:, q]).dot(self.X/WH) + sum_W[q]
+                        hess = ((self.W[:, q]**2)).dot(self.X/(WH**2))  # elementwise 2d order derivative
                     s = np.maximum(self.H[q, :] - grad/hess, eps)
                     if self.method == "SN":
                         # safe update
@@ -93,8 +128,12 @@ class Solver(BaseSolver):
                     else:
                         Hnew[q, :] = s
 
-                    WH += np.outer(self.W[:, q], Hnew[q, :] - self.H[q, :])  # updated
-                    WH = np.maximum(WH, eps)
+                    if sp.issparse(self.X):
+                        WHdata += (self.W[:, q])[i]*(Hnew[q, :] - self.H[q, :])[j]
+                        WHdata = np.maximum(WHdata, eps)
+                    else:
+                        WH += np.outer(self.W[:, q], Hnew[q, :] - self.H[q, :])  # updated
+                        WH = np.maximum(WH, eps)
                 
                 self.H = Hnew
 
