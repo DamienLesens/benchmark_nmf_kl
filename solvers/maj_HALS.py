04b42fa8696a0fdc669a334575059d345a65c470
damien <damien.lesens@orange.fr>
2026-02-09 16:35:53 +0100

    adding a variable step size for maj HALS

--- solvers/maj_HALS.py
+++ solvers/maj_HALS.py
@@ -17,6 +17,7 @@ class Solver(BaseSolver):
     parameters = {
         'n_inner_iter': [5],
         'iter_HALS': [10,20,50],
+        'gamma' : [1.9],
         'sinkhorn_init': [True],
         'sinkhorn_freq': [None]
     }
@@ -33,8 +34,7 @@ class Solver(BaseSolver):
         self.rank = rank
         self.factors_init = factors_init  # None if not initialized beforehand
 
-    @staticmethod
-    def update_maj_HALS_H(V,W,H0,I=50):
+    def update_maj_HALS_H(self, V,W,H0):
         H = H0.copy()
         R,_ = H.shape
         eps=np.finfo(float).eps
@@ -47,14 +47,14 @@ class Solver(BaseSolver):
             B2 = VoverWH2(V,W,H)
             beta = B2.max(axis=0).toarray()
             margWN = W.sum(axis=0) #size R
-            WtY = WtW@H + (W.T@ (VoverWH(V,W,H,"csc")) - np.tile(margWN,(M,1)).T)/beta
+            WtY = WtW@H + self.gamma*(W.T@ (VoverWH(V,W,H,"csc")) - np.tile(margWN,(M,1)).T)/beta
         else:
             WH = W@H
             B2 = V/(WH**2+eps)
             beta = np.max(B2,axis=0)
-            WtY = WtW@H + W.T@(V/(WH+eps)-np.ones(V.shape))/beta
+            WtY = WtW@H + self.gamma*W.T@(V/(WH+eps)-np.ones(V.shape))/beta
         
-        for it in range(I):
+        for it in range(self.iter_HALS):
             for k in range(R): 
                 num = WtY[k, :] - np.dot(WtW[k, :], H) + WtW[k, k] * H[k, :] 
                 den = WtW[k, k] 
@@ -84,11 +84,11 @@ class Solver(BaseSolver):
 
             #update H
             for _ in range(D):
-                self.H = self.update_maj_HALS_H(self.X,self.W,self.H,I=self.iter_HALS)
+                self.H = self.update_maj_HALS_H(self.X,self.W,self.H)
 
             #update W
             for _ in range(D):
-                self.W = self.update_maj_HALS_H(self.X.T,self.H.T,self.W.T,I=self.iter_HALS).T
+                self.W = self.update_maj_HALS_H(self.X.T,self.H.T,self.W.T).T
             
             it+=1
             if self.sinkhorn_freq is not None and it%self.sinkhorn_freq==0:
